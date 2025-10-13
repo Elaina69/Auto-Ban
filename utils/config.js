b4d882b9dc0aeb3a2509bea9a2e58a1d1714c250
Elaina69 <vipnoxhd123@gmail.com>
2025-10-13 16:51:53 +0700

    Fix: Add improt readline so it can use on Android Termux

--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import lang from '../configs/lang.js';
+import readline from 'readline';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
